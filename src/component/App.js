bfa024e0b514d9ca2d8ebc677dfe184a8c5b15ae
SaicharanPapani <90519153+SaicharanPapani@users.noreply.github.com>
2021-11-03 21:17:59 -0700

    Added few more functionalities like history and previous result with clear button.

--- src/component/App.js
+++ src/component/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Display from "./Display";
 import ButtonPanel from "./ButtonPanel";
+import PreviousResult from "./PreviousResult";
 import calculate from "../logic/calculate";
 import "./App.css";
 
@@ -11,15 +12,40 @@ export default class App extends React.Component {
     operation: null,
   };
 
+  toggle = true;
+  previousOperations = [];
+
   handleClick = buttonName => {
     this.setState(calculate(this.state, buttonName));
+    console.log(this.state);
+    if(this.state.total != null && this.state.next != null && this.state.operation != null){
+      this.previousOperations.push(this.state);
+    }
+    console.log(this.previousOperations);
+  };
+
+  clear = () => {
+    window.location.reload();
   };
 
   render() {
+    if(this.previousOperations.length){
+      this.previousOperations.at(this.previousOperations.length-1).result = this.state.total;
+    }
     return (
       <div className="component-app">
         <Display value={this.state.next || this.state.total || "0"} />
+        <PreviousResult value={this.state.total || "0"}/>
         <ButtonPanel clickHandler={this.handleClick} />
+          <span className="heading">History</span>
+          <button onClick={this.clear} >Clear</button>
+        <div className="panel-and-history">
+          {
+            this.previousOperations.map((x,i) =>{
+                return <><span>{x.total} {x.operation === 'x^y'? '^':x.operation} {x.next} = {x.result}</span><br /></>
+            })
+          }
+        </div>
       </div>
     );
   }
