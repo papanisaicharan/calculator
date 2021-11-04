58bc30cad4a57de63b5b48f0edbf03bd9ea2c65f
SaicharanPapani <90519153+SaicharanPapani@users.noreply.github.com>
2021-11-03 20:20:09 -0700

    x^y implementation

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -2,7 +2,8 @@ import Big from "big.js";
 
 export default function operate(numberOne, numberTwo, operation) {
   const one = Big(numberOne || "0");
-  const two = Big(numberTwo || (operation === "÷" || operation === 'x' ? "1": "0")); //If dividing or multiplying, then 1 maintains current value in cases of null
+  // addition few edit conditions for x^y
+  const two = Big(numberTwo || (operation === "÷" || operation === 'x' || operation === 'x^y' ? "1": "0")); //If dividing or multiplying, then 1 maintains current value in cases of null
   if (operation === "+") {
     return one.plus(two).toString();
   }
@@ -12,6 +13,7 @@ export default function operate(numberOne, numberTwo, operation) {
   if (operation === "x") {
     return one.times(two).toString();
   }
+  // not editing this functionality - but we can remove this, as we no longer have this functionality
   if (operation === "÷") {
     if (two === "0") {
       alert("Divide by 0 error");
@@ -20,5 +22,25 @@ export default function operate(numberOne, numberTwo, operation) {
       return one.div(two).toString();
     }
   }
+  if (operation === "x^y") {
+    if (two === "0") {
+      return "1";
+    } else if( two === "1"){
+      return one.toString();
+    }else {
+      // alert("if y is given as float, it will be converted to int!");
+      // return one.pow(parseInt(two.toString())).toString()
+      // cannot go with above approach as pow parameter is integer
+      // performing pow operation
+      var res = Math.pow(parseFloat(one.toString()), parseFloat(two.toString()));
+      // setting the precision to be in limits (ideally not required and it depends on requirements)
+      if(res.toString().length > 10){
+        return res.toPrecision(10).toString();
+      }
+      // converting to string
+      return res.toString();
+    }
+  }
+
   throw Error(`Unknown operation '${operation}'`);
 }
